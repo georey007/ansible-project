843ce832874f9b1a21634510c1e8781123542c79
georey007 <georgeysiby007@gmail.com>
2025-04-15 20:14:09 -0400

    Update outputs.tf

--- modules/linux/outputs.tf
+++ modules/linux/outputs.tf
@@ -1,1 +1,16 @@
 
+output "linux_vm_names" {
+  value = azurerm_linux_virtual_machine.linux_vm[*].name
+}
+
+output "linux_vm_private_ips" {
+  value = azurerm_linux_virtual_machine.linux_vm[*].private_ip_address
+}
+
+output "linux_vm_public_ips" {
+  value = azurerm_linux_virtual_machine.linux_vm[*].public_ip_address
+}
+
+output "linux_availability_set" {
+  value = azurerm_availability_set.linux_vm_avs.name
+}
