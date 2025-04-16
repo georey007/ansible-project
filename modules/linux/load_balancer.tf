c34309473bb279182b5616c41735e9f464dd15eb
georey007 <georgeysiby007@gmail.com>
2025-04-15 20:12:27 -0400

    Update load_balancer.tf

--- modules/linux/load_balancer.tf
+++ modules/linux/load_balancer.tf
@@ -1,1 +1,20 @@
+resource "azurerm_public_ip" "lb_pip" {
+  name                = "n01709202-lb-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = "n01709202-lb"
+}
 
+resource "azurerm_lb" "linux_lb" {
+  name                = "n01709202-lb"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "public-lb-fe"
+    public_ip_address_id = azurerm_public_ip.lb_pip.id
+  }
+}
