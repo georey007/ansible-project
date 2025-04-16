4ed1c2c073f4408c6d0a993f76c9ae9ca661d553
georey007 <georgeysiby007@gmail.com>
2025-04-15 20:15:01 -0400

    Update provisioner.tf

--- modules/linux/provisioner.tf
+++ modules/linux/provisioner.tf
@@ -1,1 +1,22 @@
 
+resource "null_resource" "linux_provisioner" {
+  count = var.nb_count
+
+  connection {
+    type        = "ssh"
+    user        = var.admin_username
+    private_key = file(var.private_key)
+    host        = azurerm_linux_virtual_machine.linux_vm[count.index].public_ip_address
+  }
+
+  provisioner "remote-exec" {
+  inline = [
+    "sudo setenforce 0",
+    "sudo yum install -y nginx",
+    "sudo systemctl enable nginx",
+    "sudo systemctl start nginx",
+    "sudo setenforce 1"
+  ]
+ }
+
+}
